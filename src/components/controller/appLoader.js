af1ecf0f4e132b08e8c2b71ede5dc5c0d4ad29c1
ConstantineTU <8kozmo8@gmail.com>
2021-12-06 02:20:54 +0500

    feat: add API key, app work

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd33991ee7c3044eb868894b0f8d1d189', // получите свой ключ https://newsapi.org/
         });
     }
 }
