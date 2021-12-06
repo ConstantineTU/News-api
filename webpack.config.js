b07f1f42e51cae3e50699d7737c86ace780154e7
ConstantineTU <8kozmo8@gmail.com>
2021-12-06 18:40:02 +0500

    feat: configured webpack, eslint and tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,19 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            }
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
