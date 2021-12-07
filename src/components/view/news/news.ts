68cb5b76ad18fe7ae5b718472aa0eaf5177fc104
ConstantineTU <8kozmo8@gmail.com>
2021-12-08 03:01:24 +0500

    fix: fix errors in sources.ts file

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,31 +1,34 @@
 import './news.css';
 
+import { ArticlesTypes } from '../../../types';
+
 class News {
-    draw(data) {
+    draw(data: Array<ArticlesTypes>) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            if (newsClone !== null) {
+                if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
+                newsClone.querySelector<HTMLDivElement>('.news__meta-photo').style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'
+                    })`;
+                newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
+                newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
+                    .slice(0, 10)
+                    .split('-')
+                    .reverse()
+                    .join('-');
 
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+                newsClone.querySelector('.news__description-title').textContent = item.title;
+                newsClone.querySelector('.news__description-source').textContent = item.source.name;
+                newsClone.querySelector('.news__description-content').textContent = item.description;
+                newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            }
 
             fragment.append(newsClone);
         });
--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,20 +1,25 @@
 import './sources.css';
 
+import { SourcesTypes } from '../../../types';
+
 class Sources {
-    draw(data) {
+    draw(data: Array<SourcesTypes>) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp');
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const sources = document.querySelector('.sources') as HTMLTemplateElement;
 
         data.forEach((item) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true);
+            const sourceItemName = (sourceClone as HTMLElement).querySelector('.source__item-name') as HTMLTemplateElement;
+            const sourceItem = (sourceClone as HTMLElement).querySelector('.source__item') as HTMLTemplateElement;
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+            sourceItemName.textContent = item.name;
+            sourceItem.setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        sources.append(fragment);
     }
 }
 
