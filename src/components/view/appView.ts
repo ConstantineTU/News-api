ac1c20020b2f675d2e6e3406841e1ad48ed6e0a3
ConstantineTU <8kozmo8@gmail.com>
2021-12-08 15:53:08 +0500

    feat: add union constructions

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -11,12 +11,12 @@ export class AppView {
         this.sources = new Sources();
     }
 
-    public drawNews(data?: Partial<INewsAndSources>) {
+    public drawNews(data: Partial<INewsAndSources> | undefined) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    public drawSources(data?: Partial<INewsAndSources>) {
+    public drawSources(data: Partial<INewsAndSources> | undefined) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
